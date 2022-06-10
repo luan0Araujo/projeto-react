8eb8e0aba3a8d916603cbc36618899cb3b15aaf7
Nathan Garcia dos Santos <nathangar01@gmail.com>
2022-06-09 23:19:44 -0300

    Logi w/ bootstrap (incompleto)

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-
+import { Modal, Button, Form } from "react-bootstrap"
 import { useForm } from "react-hook-form";
 import './csLogin.css'
 import { useContext, useState } from "react";
@@ -7,56 +7,52 @@ import { AuthContext } from "../../contexts/auth";
 
 
 export const Login = () => {
-    const {register, handleSubmit} = useForm();
-    const { login} = useContext(AuthContext);
-    const [ resp, setResp] = useState();
+    const { register, handleSubmit } = useForm();
+    const { login } = useContext(AuthContext);
+    const [resp, setResp] = useState();
     const [visible, setVisible] = useState(false)
 
     const mensagemUsuario = (msg: any) => {
 
         setResp(msg)
         setVisible(true)
-        const timer = setTimeout(() => { setVisible(false)}, 3000 )
-        
+        const timer = setTimeout(() => { setVisible(false) }, 3000)
+
         return () => clearTimeout(timer)
     }
 
     const onSubmit = async (data: any) => {
 
         const resposta = await login(data);
-        if(resposta){
-            
+        if (resposta) {
+
             mensagemUsuario(resposta)
-           
+
         }
-        
+
     }
 
     return (
-        <div className="login">
-            
-            
-            <form className="formLogin" onSubmit={handleSubmit(onSubmit)}>
-                {visible && ( <div className={'error'}>{resp}</div>)}
-                <label className="email">
-                    <span>Email </span>
-                    <input {...register("email")}/>
-                </label>
+        // <div className="login">
+        <>
 
-                <label className="senha">
-                    <span>Senha </span>
-                    <input type="password"  {...register("password")}/>
-                </label>
-                
-                <button className="entrar">
-                    Entrar
-                </button>
-                <div className="resposta">
-                    
-                </div>
-                
-            </form>
-            
+        <div className="formLogin">
+          <Form onSubmit={handleSubmit(onSubmit)}>
+                {visible && (<div className={'error'}>{resp}</div>)}
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control {...register("email")} type="text" placeholder="Email" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control {...register("password")} type="password" placeholder="Senha" required />
+                </Form.Group>
+
+                <Button variant="success" type="submit" className="btn-form-ngn">
+                        Entrar
+                    </Button>
+            </Form>
         </div>
+        </>
     );
 }
